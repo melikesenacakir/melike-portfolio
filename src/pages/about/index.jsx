6fc29dd8576b0e7e60b7e669780623bbf4f2ec4d
Melike Sena Çakır <melikesennaa@gmail.com>
2025-09-23 01:00:31 +0300

    about page created

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,12 +1,31 @@
 import { Navbar } from "../../components/navbar/navbar";
 import { Footer } from "../../components/footer/footer";
+import { useTranslation } from "react-i18next";
 
 export const About = () => {
+    const { t } = useTranslation();
     return (
-        <div className="bg-gradient-to-t from-lightBlue to-darkBlue min-h-screen flex flex-col">
-            <Navbar />
-            About
-            <Footer />
+        <div className="relative bg-gradient-to-t from-lightBlue to-darkBlue min-h-screen flex flex-col">
+            <div 
+            className="absolute inset-0 opacity-5 bg-cover bg-center bg-no-repeat blur-sm"
+            style={{
+                backgroundImage: "url('/images/code_back.gif')"
+            }} />
+            <div className="relative z-10">
+                <Navbar />
+                <div className="flex flex-col p-6 w-fit mx-auto h-screen gap-6 items-center bg-gradient-to-t from-lightBlue to-darkBlue shadow-2xl">
+                    <div className="flex flex-row gap-6 items-center justify-center space-x-6">
+                    <img src="/images/me.gif" alt="Profile" className="w-24 h-24 rounded-[20px] border-white shadow-2xl" />
+                    <div className="max-w-xl flex flex-col">
+                        <h1 className="text-6xl text-cuteBlue font-bold font-poppins mb-4">{t('about.heading')}</h1>
+                    </div>
+                    </div>
+                    <div className="max-w-2xl">
+                        <p className="font-poppins text-xl">{t('about.content')}</p>
+                    </div>
+                </div>
+                <Footer />
+            </div>
         </div>
-    );
+    )
 };
